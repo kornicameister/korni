69d5257c6463874fcf4c26a546c3348d69101dc2
Tomasz Trębski <kornicameister@gmail.com>
2017-03-06 23:41:54 +0100

    Working page layout with angular-material
    Added following elements:
    * toolbar
    * two containers (main content, short bio)
    * added code that will display list of my personal accounts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   ]
 })
 export class AppComponent {
-  readonly direction: string = "column"
+  readonly title: string = "kornicameister.github.io"
+  readonly accounts: Account[] = [
+    new Account(
+      'StackOverflow',
+      new URL('https://stackoverflow.com/users/1396508/kornicameister'),
+      'account_box'
+    )
+  ]
+}
+
+class Account {
+  constructor(public label:string, public href:URL, public icon:string){}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import './polyfills.ts'
+import 'hammerjs';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
-import './polyfills.ts'
 
 if (environment.production) {
   enableProdMode();
