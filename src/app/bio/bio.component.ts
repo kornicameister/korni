c6003ea63faa4c8fb5101a23f6f7d67cc422741c
Tomasz Trębski <kornicameister@gmail.com>
2017-03-11 23:02:01 +0100

    Adding bio component

---
+++ src/app/bio/bio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'km-bio',
+  templateUrl: './bio.component.html',
+  styleUrls: [
+    './bio.component.css'
+  ]
+})
+export class BioComponent {
+  
+}
