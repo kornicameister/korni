4707dadcaa2a41155a34ac740aae982a573a002f
kornicameister <kornicameister@gmail.com>
2017-09-30 11:34:54 +0200

    Removed old Angular2 approach, let's roll with ReactJS

--- src/app/bio/bio.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { BioComponent } from './bio.component';
-
-@NgModule({
-  exports: [ BioComponent ],
-  declarations: [ BioComponent ]
-})
-export class BioModule { }
