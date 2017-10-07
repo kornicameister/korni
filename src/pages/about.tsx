3f9b37883b2f17eb028e9e3954fbfe3d51838f33
kornicameister <kornicameister@gmail.com>
2017-10-07 23:43:35 +0200

    Add tests and refactor AboutPage
    * added couple tests to verify correctness of AboutPage
    * added dependencies to support this and future test cases
    * refactored AboutPage code to learn bit more about the way I can
      build `render()` with React

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import {
   Container,
   Jumbotron,
@@ -7,38 +8,58 @@ import {
 
 export default class AboutPage extends React.Component {
 
+  renderAvatar() {
+    let alt: string = 'kornicameister gravatar';
+    let srcSize: number = 120;
+    let src: string = `https://s.gravatar.com/avatar/140db4c0a9767838be5a5289ad78eca6?s=${srcSize}`;
+
+    return (
+      <img className="d-flex align-self-center rounded mr-3 img-fluid" alt={alt} src={src} />
+    )
+  }
+
+  renderContent() {
+
+    let paragraphs: Array<string> = [
+      'What to tell...I am a guy whose work is his pride',
+      `I am always trying to push myself and others to try and see all
+      variables of the equation as for me there's no ideal solution.
+      There is no such thing as closed door that I wouldn't
+      try to open, the only problem is the time I need to unlock it.`,
+      `I am a person who believes in all the things he can touch, smell and see.
+      Raised to think for himself and to always fight for his beliefes.
+      Never to back down, because nobody will give me what I desire.`,
+      `There are few things that I consider most important in my life,
+      yet my wife and daughter happiness is one of those things.
+      Everything I ever accomplished and will accomplish is dedicated to them.`
+    ]
+
+    return (
+      <div>
+        <h5 className="mt-0">About me</h5>
+        <div className="lead">
+          {paragraphs.map((text, index) => {
+            return <p className="text-justify" key={index} >{text}</p>
+          })}
+        </div>
+      </div>
+    )
+  }
+
   render() {
     return (
-      <Jumbotron fluid>
-        <Container fluid>
-          <Media>
-            <img className="d-flex align-self-center rounded mr-3"
-              alt="kornicameister gravatar" src="https://s.gravatar.com/avatar/140db4c0a9767838be5a5289ad78eca6?s=120" />
-            <Media body>
-              <h5 className="mt-0">About me</h5>
-              <div className="lead">
-                <p>What to tell...I am a guy whose work is his pride.</p>
-                <p>
-                  I am always trying to push myself and others to try and see all
-                    variables of the equation as for me there's no ideal solution.
-                    There is no such thing as closed door that I wouldn't
-                    try to open, the only problem is the time I need to unlock it.
-                  </p>
-                <p>
-                  I am a person who believes in all the things he can touch, smell and see.
-                    Raised to think for himself and to always fight for his beliefes.
-                    Never to back down, because nobody will give me what I desire.
-                  </p>
-                <p>
-                  There are few things that I consider most important in my life,
-                    yet my wife and daughter happiness is one of those things.
-                    Everything I ever accomplished and will accomplish is dedicated to them.
-                  </p>
-              </div>
+      <div>
+        <Jumbotron fluid>
+          <Container fluid>
+            <Media>
+              {this.renderAvatar()}
+              <Media body>
+                {this.renderContent()}
+              </Media>
             </Media>
-          </Media>
-        </Container>
-      </Jumbotron>
+          </Container>
+        </Jumbotron>
+      </div>
     )
   }
 
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,6 @@
+import { configure as EnzymeConfigure } from 'enzyme';
+import * as  Adapter from 'enzyme-adapter-react-16';
+
 // init env
 const localStorageMock = {
   getItem: jest.fn(),
@@ -6,3 +9,5 @@ const localStorageMock = {
 };
 global.localStorage = localStorageMock
 //init env
+
+EnzymeConfigure({ adapter: new Adapter() });
