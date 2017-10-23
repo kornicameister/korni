8750dbea566ef7cf925e85ac8f099cad12d77ea1
Tomasz Trębski <tomasz.trebski@ts.fujitsu.com>
2017-10-23 08:45:45 +0200

    Spinner is now React.SFC

--- src/common/spinner.tsx
+++ src/common/spinner.tsx
@@ -3,36 +3,35 @@ import * as classnames from 'classnames';
 
 import './spinner.css';
 
-export default class Spinner extends React.Component<any, any> {
-
-  private getBarStyle(i: number): object {
-    let animationDelay: string = (i - 12) / 10 + 's';
-    let transform: string = 'rotate(' + (i * 30) + 'deg) translate(146%)';
-    return {
-      'WebkitAnimationDelay': animationDelay,
-      'animationDelay': animationDelay,
-      'WebkitTransform': transform,
-      'transform': transform
-    }
-  }
-
-  render() {
-
-    const bars = []
-    const props = this.props;
-    const its: number = 12;
-
-    for (let i: number = 0; i < its; i++) {
-      bars.push(
-        <div style={this.getBarStyle(i)} className="react-spinner-bar" key={i} />
-      );
-    }
-
-    return (
-      <div {...props} className={classnames(props.className || '', 'react-spinner')}>
-        {bars}
-      </div>
-    )
-  }
-
+function getBarStyle(i: number): object {
+  let animationDelay: string = (i - 12) / 10 + 's';
+  let transform: string = 'rotate(' + i * 30 + 'deg) translate(146%)';
+  return {
+    WebkitAnimationDelay: animationDelay,
+    animationDelay: animationDelay,
+    WebkitTransform: transform,
+    transform: transform
+  };
 }
+
+const Spinner: React.SFC<any> = props => {
+  const bars = [];
+  const its: number = 12;
+
+  for (let i: number = 0; i < its; i++) {
+    bars.push(
+      <div style={getBarStyle(i)} className="react-spinner-bar" key={i} />
+    );
+  }
+
+  return (
+    <div
+      {...props}
+      className={classnames(props.className || '', 'react-spinner')}
+    >
+      {bars}
+    </div>
+  );
+};
+
+export default Spinner;
