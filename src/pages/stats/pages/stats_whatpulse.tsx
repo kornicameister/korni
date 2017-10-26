d2a6d3c2cf800862535bc4e9034cbfa94c2cd024
Tomasz Trębski <kornicameister@gmail.com>
2017-10-26 23:16:07 +0200

    First approach to WhatPulse

--- src/pages/stats/pages/stats_whatpulse.tsx
+++ src/pages/stats/pages/stats_whatpulse.tsx
@@ -1,7 +1,32 @@
 import * as React from 'react';
 
-export default class WhatPulseStats extends React.Component<any, any> {
-  public render() {
-    return <span>TBD</span>;
-  }
-}
+const WhatPulseStats: React.SFC<any> = () => {
+
+  fetch('http://api.whatpulse.org/user.php?user=kornicameister')
+    .then((resp: any) => {
+      return resp.text();
+    })
+    .then((str: string) => {
+      return new Promise((resolve, reject) => {
+        const parseString = require('xml2js').parseString;
+        parseString(str, (err: any | null, result: any) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(result.WhatPulse);
+        });
+      });
+    })
+    .then((wp: any) => {
+      return {
+        keys: Number(wp.Ranks[0].Keys[0]),
+        clicks: Number(wp.Ranks[0].Clicks[0])
+      };
+    });
+
+  return (
+    <div>WhatPulse</div>
+  );
+};
+
+export default WhatPulseStats;
