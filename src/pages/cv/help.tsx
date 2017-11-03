26d51e027e71bcab66bf63161cc053cdea006850
Tomasz Trębski <kornicameister@gmail.com>
2017-11-03 23:59:40 +0100

    Render command list

--- src/pages/cv/help.tsx
+++ src/pages/cv/help.tsx
@@ -2,7 +2,15 @@ import * as React from 'react';
 
 const CVHelp: React.SFC<{}> = (props) => {
   return (
-    <div>Help</div>
+    <div className='container-fluid'>
+      <section className='column'>
+        <div className='card border-dark mb-3'>
+          <div className='card-header'>
+            <p>Command list</p>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
