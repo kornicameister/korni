ef64aee6afe0abb23dcd00f8f7ac9dd889c94eba
Tomasz Trębski <kornicameister@gmail.com>
2017-11-05 23:05:19 +0100

    Further work, working on accordion

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { HashRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap-grid.css';
 import 'bootstrap/dist/css/bootstrap-reboot.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import 'font-awesome/css/font-awesome.css';
 import 'react-transition-group';
 import 'typeface-roboto';
--- src/pages/cv/work.tsx
+++ src/pages/cv/work.tsx
@@ -9,30 +9,52 @@ const CVWork: React.SFC<{ work: object }> = (props) => {
   // replace all dates with proper date object first
   work.forEach((w) => {
     const eDate: string = w.endDate;
+    w.startDate = moment(w.startDate);
     w.endDate = eDate ? moment(eDate) : 'now';
   });
-  work.sort((a, b) => {
-    return moment(b.startDate).milliseconds() - moment(a.startDate).milliseconds();
+  work.sort((a, b): number => {
+    return moment(a.startDate).diff(b.startDate);
   });
 
   return (
     <div className='container-fluid'>
-      {
-        work.map((w: any, key: number) => {
-          return (
-            <section key={key} className='col'>
-              <div className='card border-dark mb-3'>
-                <div className='card-header'>
-                  <h2>{w.company}</h2>
-                  <h3>{moment(w.startDate).format(DATE_FORMAT)} - {moment(w.endDate).format(DATE_FORMAT)}</h3>
+      <div id='work-accordion' role='tablist' aria-label='work'>
+        {
+          work.map((w: any, key: number) => {
+            return (
+              <section id={'collapse' + key} key={key} className='col collapse show'>
+                <div className='card border-dark mb-3 text-justify'>
+                  <div className='card-header' aria-label='company'>
+                    <h4 className='mb-0 pull-left'><a href={w.website}>{w.company}</a></h4>
+                    <a className='pull-right' data-toggle='collapse' href={'#collapse' + key} aria-expanded='true' aria-controls={'collapse' + key}>!</a>
+                  </div>
+                  <div className='card-header' aria-label='dates'>
+                    <h5 className='mb-0'>{moment(w.startDate).format(DATE_FORMAT)} - {moment(w.endDate).format(DATE_FORMAT)}</h5>
+                  </div>
+                  <div className='card-header' aria-label='position'>
+                    <h6 className='mb-0'>{w.position}</h6>
+                  </div>
+                  {w.summary && (<div className='card-body'>
+                    <p className='card-text'>
+                      {w.summary}
+                    </p>
+                  </div>
+                  )}
+                  {
+                    w.highlights && w.highlights.map((h: any, hKey: number) => {
+                      return (
+                        <ul key={hKey * key} className='list-group list-group-flush'>
+                          <li className='list-group-item'><span className='card-text'>{h}</span></li>
+                        </ul>
+                      );
+                    })
+                  }
                 </div>
-                <div className='card-body'>
-                </div>
-              </div>
-            </section>
-          );
-        })
-      }
+              </section>
+            );
+          })
+        }
+      </div>
     </div>
   );
 };
