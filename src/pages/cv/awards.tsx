c1fa3cbe11eeb47be4f86c2c40e47c22e64d5d1a
Tomasz Trębski <kornicameister@gmail.com>
2017-11-12 14:11:03 +0100

    Couple styling tweaks (#125)

--- src/pages/cv/awards.tsx
+++ src/pages/cv/awards.tsx
@@ -17,7 +17,7 @@ const CVAwards: React.SFC<{ awards: Award[] }> = (props) => {
             <div key={'award-' + index} className='card dark-border mb-2'>
               <div className='card-header'>
                 <h5 className='card-title text-success pull-left'>{award.title} in {award.date}</h5>
-                <h5 className='card-subtitle pull-right'>{award.awarder}</h5>
+                <h5 className='card-title pull-right'>{award.awarder}</h5>
               </div>
               <div className='card-body'>
                 <div className='card-text'>{award.summary}</div>
