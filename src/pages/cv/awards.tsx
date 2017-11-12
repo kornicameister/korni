bdb7ec3f650a3a106452ed6115f825739cfd082a
Tomasz Trębski <kornicameister@gmail.com>
2017-11-12 13:23:56 +0100

    Render resume awards

--- src/pages/cv/awards.tsx
+++ src/pages/cv/awards.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
 
-const CVAwards: React.SFC<{ awards: object }> = (props) => {
+interface Award {
+  awarder: string;
+  date: string;
+  summary: string;
+  title: string;
+}
+
+const CVAwards: React.SFC<{ awards: Award[] }> = (props) => {
+  const { awards } = props;
   return (
-    <div>Awards</div>
+    <div className='container'>
+      {
+        awards.map((award: Award, index: number) => {
+          return (
+            <div key={'award-' + index} className='card dark-border mb-2'>
+              <div className='card-header'>
+                <h5 className='card-title text-success pull-left'>{award.title} in {award.date}</h5>
+                <h5 className='card-subtitle pull-right'>{award.awarder}</h5>
+              </div>
+              <div className='card-body'>
+                <div className='card-text'>{award.summary}</div>
+              </div>
+            </div>
+          );
+        })
+      }
+    </div>
   );
 };
 
