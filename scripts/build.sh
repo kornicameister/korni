a4ec92bcffdce521af4a566fdc6ec900fdbe1186
Tomasz Trębski <kornicameister@gmail.com>
2017-12-31 14:15:49 +0100

    Don't expose 3rd party secrets
    - use environmental variables to load any 3rd party secrets
    - simplify verify stage to run all verify scripts in one stage

---
+++ scripts/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# constants
+DOT_ENV=${PWD}/.env.production
+
+# variables injected from travis environmental variables
+REACT_APP_GA_KEY=${KORNI_GA_ID}
+
+# helper functions
+update_dot_env() {
+  echo ">>> Creating ${DOT_ENV} file for producation build"
+
+  cat >"${DOT_ENV}" <<EOL
+REACT_APP_GA_KEY=${REACT_APP_GA_KEY}
+EOL
+
+}
+
+check_env() {
+  echo ">>> Checking required environmental variable presence"
+
+  if [ -z "$KORNI_GA_ID" ]; then
+    die ">>> - \"KORNI_GA_ID\" is empty"
+  fi
+}
+
+build() {
+  echo ">>> Running production build"
+
+  react-scripts-ts build
+}
+
+clean() {
+  echo ">>> Cleaning ${DOT_ENV}"
+  rm -rf $DOT_ENV
+}
+
+die() {
+  echo "$*" 1>&2 ; exit 1;
+}
+
+echo ">>> Running build script" && check_env && update_dot_env && build && clean
