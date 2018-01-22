0cd40aeee32ddafe44e3515973550e4af319cda2
Tomasz Trębski <kornicameister@gmail.com>
2018-01-22 23:28:24 +0100

    Add displaying public contribution summary

--- src/pages/stats/pages/stats_github.tsx
+++ src/pages/stats/pages/stats_github.tsx
@@ -8,10 +8,35 @@ interface GithubContribution {
   repo: string;
   isFork: boolean;
   isPrivate: boolean;
+  issueCount: {
+    total: number;
+    authored: number;
+  };
+  commitsCount: {
+    total: number;
+    authored: number;
+  };
+  pullRequestCount: {
+    total: {
+      open: number;
+      merged: number;
+    };
+    authored: {
+      open: number;
+      merged: number;
+    };
+  };
 }
 
 interface Contributions {
   [kind: string]: GithubContribution[];
+}
+
+interface ContributionSummary {
+  repo: string;
+  commits: number;
+  pullRequests: number;
+  issues: number;
 }
 
 interface Meta {
@@ -42,16 +67,95 @@ const GithubContributionsStats = firebase.withCollection(
         : {};
 
       return (
-        <div className="text-center">
-          {Object.keys(contributions).map(kind => {
-            const contribs = contributions[kind];
-            return (
-              <div key={`contributions-${kind}`}>
-                {kind === 'public' ? 'Public' : 'Private'} contributions :{' '}
-                <strong>{contribs.length}</strong>
+        <div className="container-fluid">
+          <div className="row">
+            {Object.keys(contributions).map(kind => {
+              const contribs = contributions[kind];
+              return (
+                <div key={`contributions-${kind}`} className="col-sm text-center">
+                  {kind === 'public' ? 'Public' : 'Private'} contributions :{' '}
+                  <strong className="text-success">{contribs.length}</strong>
+                </div>
+              );
+            })}
+          </div>
+          <hr />
+          <div className="row">
+            <section className="container-fluid">
+              <div className="text-center">
+                <h4>Public contributions for all repositories</h4>
               </div>
-            );
-          })}
+              <div className="card-deck mx-auto">
+                {(contributions['public'] || [])
+                  .reduce(
+                    (
+                      summary: ContributionSummary[],
+                      contribution: GithubContribution,
+                    ) => {
+                      const repo = contribution.repo;
+                      const repoSummary = summary.find(s => s.repo === contribution.repo);
+                      if (repoSummary) {
+                        repoSummary.issues += contribution.issueCount.authored;
+                        repoSummary.commits += contribution.commitsCount.authored;
+                        repoSummary.pullRequests +=
+                          contribution.pullRequestCount.authored.open +
+                          contribution.pullRequestCount.authored.merged;
+                      } else {
+                        summary.push({
+                          repo,
+                          commits: contribution.commitsCount.authored,
+                          issues: contribution.issueCount.authored,
+                          pullRequests:
+                            contribution.pullRequestCount.authored.open +
+                            contribution.pullRequestCount.authored.merged,
+                        });
+                      }
+                      return summary;
+                    },
+                    [] as ContributionSummary[],
+                  )
+                  .filter(
+                    repoSummary =>
+                      repoSummary.commits > 0 &&
+                      repoSummary.issues > 0 &&
+                      repoSummary.pullRequests > 0,
+                  )
+                  .map(repoSummary => (
+                    <div key={repoSummary.repo} className="col-sm-6">
+                      <div className="card">
+                        <div className="card-header text-center">
+                          <h5 className="card-title">{repoSummary.repo}</h5>
+                        </div>
+                        <div className="card-body text-justify">
+                          <div className="card-text">
+                            <ul className="list-group list-group-flush">
+                              <li className="list-group-item">
+                                Commits:{' '}
+                                <span className="badge badge-pill badge-dark">
+                                  {repoSummary.commits}
+                                </span>
+                              </li>
+                              <li className="list-group-item">
+                                Issues:{' '}
+                                <span className="badge badge-pill badge-dark">
+                                  {repoSummary.issues}
+                                </span>
+                              </li>
+                              <li className="list-group-item">
+                                Pull requests:{' '}
+                                <span className="badge badge-pill badge-dark">
+                                  {repoSummary.pullRequests}
+                                </span>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </section>
+          </div>
         </div>
       );
     }
@@ -66,6 +170,24 @@ const GithubContributionsStats = firebase.withCollection(
     repo: doc.data()['repo'],
     isFork: doc.data()['is_fork'],
     isPrivate: doc.data()['is_private'],
+    issueCount: {
+      total: doc.data()['issues_count']['total'],
+      authored: doc.data()['issues_count']['authored'],
+    },
+    commitsCount: {
+      total: doc.data()['commits_count']['total'],
+      authored: doc.data()['commits_count']['authored'],
+    },
+    pullRequestCount: {
+      total: {
+        open: doc.data()['pull_request_count']['total']['open'],
+        merged: doc.data()['pull_request_count']['total']['merged'],
+      },
+      authored: {
+        open: doc.data()['pull_request_count']['authored']['open'],
+        merged: doc.data()['pull_request_count']['authored']['merged'],
+      },
+    },
   }),
 );
 
@@ -92,6 +214,7 @@ export default class GithubStats extends React.Component<any, any> {
     return (
       <div className="github">
         <Meta />
+        <hr />
         <GithubContributionsStats />
       </div>
     );
