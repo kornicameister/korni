96bc738f1dc25a7192e77c77b10c65ee3b3005da
Tomasz Trębski <kornicameister@gmail.com>
2018-11-03 22:54:12 +0100

    Fix error in firebase.ts

--- src/data/firebase.tsx
+++ src/data/firebase.tsx
@@ -4,20 +4,11 @@ import * as firebase from 'firebase/app';
 import * as firestore from './store';
 
 /**
- * Remove the variants of the second union of string literals from
- * the first.
- *
- * @see https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-307871458
- */
-type Diff<T extends string, U extends string> = ({ [P in T]: P } &
-  { [P in U]: never } & { [x: string]: never })[T];
-
-/**
  * Drop keys `K` from `T`.
  *
  * @see https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-307871458
  */
-type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
 export interface FirestoreComponentProps<T> {
   data?: T;
