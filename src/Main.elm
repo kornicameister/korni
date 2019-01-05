899665fc9b4fe5fd0406c835c00fd9682772e910
Tomasz Trębski <kornicameister@gmail.com>
2019-01-05 01:02:59 +0100

    Target layout (#640)

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 import Browser as B
 import Html as H
 import Html.Attributes as A
+import Task
+import View.Navigation
 
 
 
@@ -10,25 +12,22 @@ import Html.Attributes as A
 
 
 type alias Model =
-    {}
+    { version : String
+    , navigationModel : View.Navigation.Model
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+init : String -> List String -> ( Model, Cmd Msg )
+init version trianglifyDataUris =
+    let
+        ( navigationModel, navigationMsg ) =
+            View.Navigation.init trianglifyDataUris
+    in
+    ( { version = version
+      , navigationModel = navigationModel
+      }
+    , navigationMsg |> Cmd.map NavigationMsg
+    )
 
 
 
@@ -38,22 +37,45 @@ update msg model =
 view : Model -> H.Html Msg
 view model =
     H.main_ []
-        [ H.header [] []
-        , H.section [] []
-        , H.input [] []
-        , H.footer [] []
+        [ H.header []
+            [ H.h1 [] [ H.strong [] [ [ "korni", model.version ] |> String.join "@" |> H.text ] ]
+            ]
+        , H.section [ A.class "logo" ]
+            [ H.img [ A.src "%PUBLIC_URL%/logo.png" ] []
+            ]
+        , View.Navigation.view model.navigationModel
+            |> H.map NavigationMsg
         ]
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NavigationMsg View.Navigation.Msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NavigationMsg innerMsg ->
+            let
+                ( nextModel, nextMsg ) =
+                    View.Navigation.update innerMsg model.navigationModel
+            in
+            ( { model | navigationModel = nextModel }, nextMsg |> Cmd.map NavigationMsg )
 
 
 
 ---- PROGRAM ----
 
 
-main : Program () Model Msg
+main : Program { version : String, trianglifyDataUris : List String } Model Msg
 main =
     B.element
         { view = view
-        , init = \_ -> init
+        , init = \{ version, trianglifyDataUris } -> init version trianglifyDataUris
         , update = update
         , subscriptions = always Sub.none
         }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// libs
+import * as Trianglify from 'trianglify';
+
 // styles
 import 'normalize.css';
 import './main.css';
@@ -6,8 +9,27 @@ import './main.css';
 import { Elm } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
+// generate startup patterns
+const PATTERN_COUNT = 32;
+const VERSION =
+  process.env.NODE_ENV === 'development' ? 'dev' : process.env.ELM_APP_VERSION;
+
+let patternsDataUris = [];
+for (let i = 0; i < PATTERN_COUNT; i++) {
+  let pattern = Trianglify({
+    height: 500,
+    width: 500,
+    seed: VERSION == 'dev' ? new Date() : VERSION,
+  }).png();
+  patternsDataUris.push(pattern);
+}
+
 Elm.Main.init({
   node: document.getElementById('root'),
+  flags: {
+    trianglifyDataUris: patternsDataUris,
+    version: process.env.NODE_ENV === 'development' ? 'dev' : process.env.ELM_APP_VERSION,
+  },
 });
 
 registerServiceWorker();
