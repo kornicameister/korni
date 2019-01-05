4ed1331b3956c047d405121350b20e950e6fbc3a
Tomasz Trębski <kornicameister@gmail.com>
2019-01-05 01:01:17 +0100

    fixup! Update lock

--- src/index.js
+++ src/index.js
@@ -11,23 +11,24 @@ import registerServiceWorker from './registerServiceWorker';
 
 // generate startup patterns
 const PATTERN_COUNT = 32;
-const VERSION = process.env.NODE_ENV === 'development' ? 'dev' : process.env.ELM_APP_VERSION;
+const VERSION =
+  process.env.NODE_ENV === 'development' ? 'dev' : process.env.ELM_APP_VERSION;
 
 let patternsDataUris = [];
-for(let i=0 ; i < PATTERN_COUNT ; i++){
-    let pattern = Trianglify({
-        height: 500,
-        width: 500,
-        seed: VERSION == 'dev' ? new Date() : VERSION,
-    }).png();
-    patternsDataUris.push(pattern);
+for (let i = 0; i < PATTERN_COUNT; i++) {
+  let pattern = Trianglify({
+    height: 500,
+    width: 500,
+    seed: VERSION == 'dev' ? new Date() : VERSION,
+  }).png();
+  patternsDataUris.push(pattern);
 }
 
 Elm.Main.init({
   node: document.getElementById('root'),
   flags: {
-      trianglifyDataUris: patternsDataUris,
-      version: process.env.NODE_ENV === 'development' ? 'dev' : process.env.ELM_APP_VERSION,
+    trianglifyDataUris: patternsDataUris,
+    version: process.env.NODE_ENV === 'development' ? 'dev' : process.env.ELM_APP_VERSION,
   },
 });
 
