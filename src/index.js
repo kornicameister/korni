9985dca9fed5e9c4bece26e02f455bc448fc1202
Tomasz Trębski <kornicameister@gmail.com>
2019-03-26 23:27:26 +0100

    Wakatime profile in footer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// libs
+import jsonp from 'jsonp';
+
 // styles
 import 'normalize.css';
 import './main.css';
@@ -9,9 +12,30 @@ import registerServiceWorker from './registerServiceWorker';
 const VERSION =
   process.env.NODE_ENV === 'development' ? 'dev' : process.env.ELM_APP_VERSION;
 
-Elm.Main.init({
+const app = Elm.Main.init({
   node: document.getElementById('root'),
   flags: VERSION,
 });
-
 registerServiceWorker();
+
+setTimeout(() => {
+  [
+    [
+      'languages',
+      'https://wakatime.com/share/@kornicameister/7d61cd0d-5c94-4030-83d0-9d88e86e1074.json',
+    ],
+    [
+      'editors',
+      'https://wakatime.com/share/@kornicameister/dd317183-0f14-41e8-8668-61779de7bb70.json',
+    ],
+  ].map(entry => {
+    let category = entry[0];
+    let url = entry[1];
+    jsonp(url, (err, data) => {
+      if (!!err) {
+        return;
+      }
+      app.ports.receiveWakatimeStats.send({ category, data });
+    });
+  });
+}, 1000);
